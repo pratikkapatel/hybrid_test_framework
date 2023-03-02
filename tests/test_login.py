1c80143c9907f944600afcd5b513e9e596c076a3
pratik <pratik.kapatel@einfochips.com>
2023-03-02 12:09:54 +0530

    added new method test_header

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
 
 class TestLoginUI:
     def test_title(self):
@@ -10,3 +11,14 @@ class TestLoginUI:
         act_title=driver.title
         #assert act_title=="OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(act_title)
+
+    def test_title(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        act_title = driver.find_element(By.XPATH, "//h5[normalize-space()='Login']").text
+        assert_that("Login").is_equal_to(act_title)
+
+
+        #assert act_title=="OrangeHRM"
