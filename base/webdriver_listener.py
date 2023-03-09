9160a0e5d33bea366c43c9fcd78a012681819d90
pratik <pratik.kapatel@einfochips.com>
2023-03-09 11:36:45 +0530

    updated new changes in framework and tests

---
+++ base/webdriver_listener.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+class WebDriverWrapper:
+    driver = None
+
+    @pytest.fixture(scope="function", autouse=True)
+    def browser_config(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,28 +1,39 @@
 import pytest
-from selenium import webdriver
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
-class TestLoginUI:
+from base.webdriver_listener import WebDriverWrapper
+from utilities import data_source
 
-    @pytest.fixture(scope="function",autouse=True)
-    def browser_config(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(10)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        yield
-        self.driver.quit()
 
+class TestLogin(WebDriverWrapper):
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+    @pytest.mark.parametrize("username, password, expected_error", data_source.test_invalid_login_data)
+    def test_invalid_login(self, username, password, expected_error):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_error = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Invalid')]").text
+        assert_that(expected_error).is_equal_to(actual_error)
+
+
+class TestLoginUI(WebDriverWrapper):
     def test_title(self):
-        act_title=self.driver.title
-        #assert act_title=="OrangeHRM"
-        assert_that("OrangeHRM").is_equal_to(act_title)
+        actual_title = self.driver.title
+        assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        act_header = self.driver.find_element(By.XPATH, "//h5[normalize-space()='Login']").text
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(actual_header)
 
-        assert_that("Login").is_equal_to(act_header)
-
-
-        #assert act_title=="OrangeHRM"
+    def test_login_placeholders(self):
+        actual_username_placeholder = self.driver.find_element(By.NAME, "username").get_attribute("placeholder")
+        actual_password_placeholder = self.driver.find_element(By.NAME, "password").get_attribute("placeholder")
+        assert_that("Username").is_equal_to(actual_username_placeholder)
+        assert_that("Password").is_equal_to(actual_password_placeholder)
